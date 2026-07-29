f0a79185c48aa9c50e93112b0b45a764db776572
stevanoswh <stevanoswh@gmail.com>
2026-07-29 08:24:33 +0700

    complete with build

--- src/main/java/com/superpickaxe/FireSword.java
+++ src/main/java/com/superpickaxe/FireSword.java
@@ -10,32 +10,5 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(FireSword.MOD_ID)
 public class FireSword {
 
-    public static final String MOD_ID = "firstmod";
-
-    @Mod.EventBusSubscriber(
-            modid = MOD_ID,
-            bus = Mod.EventBusSubscriber.Bus.FORGE
-    )
-    public static class ForgeEvents {
-
-        @SubscribeEvent
-        public static void onPlayerAttack(AttackEntityEvent event) {
-
-            Player player = event.getEntity();
-
-            if (player.level().isClientSide()) {
-                return;
-            }
-
-            if (!player.getMainHandItem().is(Items.DIAMOND_SWORD)) {
-                return;
-            }
-
-            if (!(event.getTarget() instanceof LivingEntity target)) {
-                return;
-            }
-
-            target.setSecondsOnFire(5);
-        }
-    }
+ 
 }
